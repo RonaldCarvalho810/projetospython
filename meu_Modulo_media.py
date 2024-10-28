412c23bbfacc78e622a45235308af9948b13d560
Ronaldcarvalho810 <ronald.carvalho.jf>
2024-10-28 16:36:10 -0300

    ultimos_exercicios

---
+++ meu_Modulo_media.py
@@ -0,0 +1,14 @@
+# calculos.py
+
+def calcular_media(numeros):
+    """
+    Calcula a média de uma lista de números.
+    :param numeros: lista de números (int ou float)
+    :return: média dos números
+    """
+    if len(numeros) == 0:
+        return 0  # Evita divisão por zero
+
+    soma = sum(numeros)
+    media = soma / len(numeros)
+    return media
---
+++ teste_modulo.py
@@ -0,0 +1,7 @@
+
+import meu_Modulo_media
+
+numeros = [10, 20, 30, 40, 50]
+
+media = meu_Modulo_media.calcular_media(numeros)
+print(f"A média dos números é: {media:.2f}")
