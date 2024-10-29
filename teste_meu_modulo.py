7336018965053924c4e2dcc18ec8c0ba3da2190b
Ronaldcarvalho810 <ronald.carvalho.jf>
2024-10-29 13:57:34 -0300

    exercicios 291024

---
+++ teste_meu_modulo.py
@@ -0,0 +1,10 @@
+# main.py
+
+import meu_modulo_media
+
+# Exemplo de lista de números
+numeros = [50, 70, 90, 11, 19]
+
+# Calculando a média usando a função do módulo
+media = meu_modulo_media.calcular_media(numeros)
+print(f"A média dos números é: {media:.2f}")
