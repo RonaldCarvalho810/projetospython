66b491789138d6862f04719a730835d7dcbf43f0
Ronaldcarvalho810 <ronald.carvalho.jf>
2024-10-30 13:58:31 -0300

    ajustes exercicios

--- aula06_cont_variaveis/aula06_Exercicios_atividade_002/exercicio_pg_29_B.py
+++ aula06_cont_variaveis/aula06_Exercicios_atividade_002/exercicio_pg_29_B.py
@@ -6,7 +6,7 @@ import os
 os.system("cls")
 
 print("=" * 20)
-print("modulo para calcular a equação (𝑥²−6𝑥+5) ")
+print("modulo para calcular a equação (x²-6x+5) ")
 print("=" * 20)
 print("regra para calculo das raizes.: ax² + bx + c =0 ")
 #atribuindo valores das mariaveis
@@ -19,10 +19,7 @@ primeira_raiz = 0
 segunda_raiz = 0
 
 #calculando o delta
-
 delta = b**2 - (4*a*c)
-
-#print(f"o valor de delta é..: {delta}")
 
 primeira_raiz = (-(b) + (delta **(1/2) )) / 2*a
 segunda_raiz = (-(b) - (delta **(1/2) )) / 2*a
@@ -30,6 +27,6 @@ segunda_raiz = (-(b) - (delta **(1/2) )) / 2*a
 print("=" * 20)
 print("resultado")
 print("=" * 20)
-print(f"as raizes da equação..: (𝑥²−6𝑥+5=0) são  𝑥’ ={primeira_raiz} e 𝑥’’ ={segunda_raiz} ")
+print(f"as raizes da equação..: (x²-6x+5=0) são  x¹={primeira_raiz} e x²={segunda_raiz} ")
 print("=" * 20)
 print()
