6cd632d2fe824c96432146f10e34b5f91bb374a3
Ronaldcarvalho810 <ronald.carvalho.jf>
2024-10-31 16:11:30 -0300

    ajustes

--- meustestes/funcoes_teste/teste_meu_modulo_media.py
+++ meustestes/funcoes_teste/teste_meu_modulo_media.py
@@ -2,7 +2,14 @@
 
 import meu_modulo_media
 
-numeros = [50, 70, 90, 11, 19, 25, 100]
+nummero1 = int(input("Digite o primeiro numero"))
+nummero2 = int(input("Digite o segundo numero"))
+nummero3 = int(input("Digite o terceiro numero"))
+nummero4 = int(input("Digite o quarto numero"))
+nummero5 = int(input("Digite o quinto numero"))
+
+
+numeros = [nummero1, nummero2, nummero3, nummero4, nummero5]
 
 # Calculando a média usando a função do meu módulo
 media = meu_modulo_media.calcular_media(numeros)
