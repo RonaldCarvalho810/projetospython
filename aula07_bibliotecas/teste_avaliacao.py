44d47c1eaebede13e7276207783491d7126c259c
Ronaldcarvalho810 <ronald.carvalho.jf>
2024-11-01 14:56:00 -0300

    praticando

--- aula07_bibliotecas/teste_avaliacao.py
+++ aula07_bibliotecas/teste_avaliacao.py
@@ -12,22 +12,25 @@ print("=" *35)
 numero2 = int(input("digite um numero..:"))
 numero3= int(input("digite um numero..:"))
 
-if(numero2>numero3 and numero2 !=10):
+if(numero2>numero3 and numero2 !=10) or (numero2== 1 and numero3==2):
     print("condicional positiva")
 else:
     print("condicao nao atendida")
 
-print("fim")
+print("============================fim===================================")
 
-valor = float(input("digite o valor"))
-desconto = float(input("digite o desconto"))
+valor = float(input("digite o valor.: "))
+porcentagem = float(input("digite a porcentagem.: "))
 
-porcentagem_de_desconto = (100 - desconto) /100
+porcentagem_de_desconto = (100 - porcentagem) /100
 
 valor_final = (valor * porcentagem_de_desconto)
 
 print(f"o valor com desconto apçicado será de..: {valor_final}")
 
-valor_acrecido = valor * (1 + (desconto / 100 ))
+valor_acrescido = valor * (1 + (porcentagem / 100 ))
 
-print(f"o valor com desconto apçicado será de..: {valor_acrecido}")
+print(f"o valor com acrescimo apçicado será de..: {valor_acrescido}")
+print()
+print()
+print()
