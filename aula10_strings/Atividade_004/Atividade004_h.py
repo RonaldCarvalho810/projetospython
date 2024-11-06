ebefbc306ef3909600ad9cf88a3fabf6d01ac488
ronaldcarvalho810 <ronald.carvalho.jf@gmail.com>
2024-11-06 01:42:58 -0300

    testes

--- aula10_strings/Atividade_004/Atividade004_h.py
+++ aula10_strings/Atividade_004/Atividade004_h.py
@@ -13,18 +13,33 @@ letras_nome = 0
 posicao_primeiro_o = 0
 posicao_ultimo_o = 0
 lista_letras = ""
+string_resto = ""
+posicao_ultimo_o_quebra = 0
+posicao_ultimo_o_quebra_final = 0
 
 #atribuindo variaveis
 nome = input("digite o nome.: ")
 
+#utilizando string invertida
 letras_nome = len(nome)
 quantidade_o = nome.count("o")
-lista_letras = list(nome)
+#lista_letras = list(nome)
 posicao_primeiro_o = nome.find("o") +1
 invertida = nome[::-1]
 posicao_ultimo_o = invertida.find("o") + letras_nome
 
+#utilizando quebra de string
+string_resto = nome[posicao_primeiro_o : letras_nome]
+posicao_ultimo_o_quebra = string_resto.find("o") +1
+posicao_ultimo_o_quebra_final = posicao_primeiro_o + posicao_ultimo_o_quebra
 
+#-----------------------------------
+#testando saidas
+#print(string_resto)
+#print(posicao_ultimo_o_quebra_final)
+#-----------------------------------
+
+#-----------------------------------
 #teste
 #print (nome)
 #print (letras_nome)
@@ -32,8 +47,13 @@ posicao_ultimo_o = invertida.find("o") + letras_nome
 #print (lista_letras)
 #print (posicao_primeiro_o)
 #print (posicao_ultimo_o)
+#-----------------------------------
 
 #saida dos dados
 print("=" *20)
+print("utilizando inversao de string")
 print(f"O nome.: {nome} , possui {quantidade_o} o, o primeiro na posicao.: {posicao_primeiro_o} e o ultimo na posicao.: {posicao_ultimo_o}")
+print("=" *20 )
+print("utilizando quebra de string")
+print(f"O nome.: {nome} , possui {quantidade_o} o, o primeiro na posicao.: {posicao_primeiro_o} e o ultimo na posicao.: {posicao_ultimo_o_quebra_final}")
 print("=" *20 )
