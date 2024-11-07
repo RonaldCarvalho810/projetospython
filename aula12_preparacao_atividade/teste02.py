1086ead50f6a4cf438e90dde1a3a1dedbb512862
RonaldCarvalho810 <ronald.carvalho.jf@gmail.com>
2024-11-07 14:22:54 -0300

    teste

---
+++ aula12_preparacao_atividade/teste02.py
@@ -0,0 +1,33 @@
+import os
+os.system("cls")
+
+def gerar_escala_formatada(texto):
+    # Criar uma lista para armazenar os índices
+    indices = []
+
+    # Iterar sobre o texto e coletar os índices dos caracteres
+    for i, letra in enumerate(texto):
+        indices.append(i)
+
+    # Construir a linha com os caracteres do texto
+    linha_texto = " ".join(texto)
+
+    # Criar a primeira linha (dezenas) e a segunda linha (unidades)
+    linha_dezenas = [str(i // 10) for i in indices]  # Parte das dezenas
+    linha_unidades = [str(i % 10) for i in indices]  # Parte das unidades
+
+    # Converter as listas para string e juntar com espaços
+    linha_dezenas = " ".join(linha_dezenas)
+    linha_unidades = " ".join(linha_unidades)
+
+    # Retornar as duas linhas formatadas
+    return linha_dezenas, linha_unidades, linha_texto
+
+# Exemplo de uso
+texto = input("digite um texto.: ")
+linha_dezenas, linha_unidades, linha_texto = gerar_escala_formatada(texto)
+
+# Mostrar o resultado
+print(linha_dezenas)
+print(linha_unidades)
+print(linha_texto)
