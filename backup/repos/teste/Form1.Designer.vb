a885678d193c9b2aabf2ec05b99035760c97a2d0
RonaldCarvalho810 <ronald.carvalho.jf@gmail.com>
2024-12-02 13:53:14 -0300

    backup

---
+++ backup/repos/teste/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(37, 311)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Adicionar"
+        Button1.UseVisualStyleBackColor = True
+        Button1.Visible = False
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(12, 90)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(233, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Showcard Gothic", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(236, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(237, 26)
+        Label1.TabIndex = 3
+        Label1.Text = "Cadastro de clientes"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(37, 340)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 4
+        Button2.Text = "Remover"
+        Button2.UseVisualStyleBackColor = True
+        Button2.Visible = False
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(251, 90)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 5
+        Button3.Text = "Validar"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(37, 369)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 6
+        Button4.Text = "Conferir"
+        Button4.UseVisualStyleBackColor = True
+        Button4.Visible = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 74)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(26, 15)
+        Label3.TabIndex = 8
+        Label3.Text = "Cpf"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label3)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ backup/repos/teste/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System.Buffers
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim i As Integer
+        Dim soma As Long
+        Dim resto As Integer
+        Dim digito1 As Integer
+        Dim digito2 As Integer
+        Dim pesos1(8) As Integer
+        Dim pesos2(9) As Integer
+        Dim cpf As String
+
+        ' Captura e limpa o CPF (removendo pontos e traços)
+        cpf = Replace(TextBox2.Text, ".", "")
+        cpf = Replace(cpf, "-", "")
+        cpf = Trim(cpf)
+
+        ' Verifica se o CPF tem 11 dígitos
+        If Len(cpf) <> 11 Then
+            MsgBox("O CPF precisa ter 11 dígitos.", vbExclamation, "Erro")
+            Exit Sub
+        End If
+
+        ' Verifica se todos os dígitos são iguais (exemplo: 11111111111)
+        If cpf = New String(cpf(0), 11) Then
+            MsgBox("CPF inválido! Todos os dígitos são iguais.", vbExclamation, "Erro")
+            Exit Sub
+        End If
+
+        ' Define os pesos para o cálculo dos dígitos verificadores
+        For i = 0 To 8
+            pesos1(i) = 10 - i
+        Next
+        For i = 0 To 9
+            pesos2(i) = 11 - i
+        Next
+
+        ' Cálculo do primeiro dígito verificador
+        soma = 0
+        For i = 1 To 9
+            soma = soma + CInt(Mid(cpf, i, 1)) * pesos1(i - 1)
+        Next
+        resto = soma Mod 11
+        If resto < 2 Then
+            digito1 = 0
+        Else
+            digito1 = 11 - resto
+        End If
+
+        ' Cálculo do segundo dígito verificador
+        soma = 0
+        For i = 1 To 10
+            If i = 10 Then
+                soma = soma + digito1 * pesos2(i - 1)
+            Else
+                soma = soma + CInt(Mid(cpf, i, 1)) * pesos2(i - 1)
+            End If
+        Next
+        resto = soma Mod 11
+        If resto < 2 Then
+            digito2 = 0
+        Else
+            digito2 = 11 - resto
+        End If
+
+        ' Verifica os dígitos verificadores
+        If Mid(cpf, 10, 1) = CStr(digito1) And Mid(cpf, 11, 1) = CStr(digito2) Then
+            MsgBox("CPF válido!", vbInformation, "Resultado")
+        Else
+            MsgBox("CPF inválido!", vbExclamation, "Resultado")
+        End If
+    End Sub
+
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If TextBox2.Text = "renato" Then
+            MsgBox("brabo")
+        Else
+            MsgBox("mansinho")
+        End If
+    End Sub
+
+End Class
