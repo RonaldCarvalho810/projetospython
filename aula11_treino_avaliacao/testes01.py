90cb5700c1350c45f5f69968ce09fb30b7fd69a2
Ronaldcarvalho810 <ronald.carvalho.jf@gmail.com>
2025-02-26 14:28:13 -0300

    teste

--- aula11_treino_avaliacao/testes01.py
+++ aula11_treino_avaliacao/testes01.py
@@ -3,7 +3,17 @@ import os
 os.system("cls")
 
 numero = 0
+mult = 0
+div = 0
+pot = 0
 
-numero = int(input("digite um numero"))
+numero = int(input("digite um numero.: "))
+
+mult = numero * 2
+div = numero / 2
+pot = numero ** 2
 
 print ( numero)
+print (mult)
+print (div)
+print (pot)
