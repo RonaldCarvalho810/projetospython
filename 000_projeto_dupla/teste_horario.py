d8034672ca725e08086c574c73c7f5d9b1c376a6
RonaldCarvalho810 <ronald.carvalho.jf@gmail.com>
2025-04-14 13:16:24 -0300

    teste 3 mobile

---
+++ 000_projeto_dupla/teste_horario.py
@@ -0,0 +1,22 @@
+from kivymd.app import MDApp
+
+try:
+    from kivymd.uix.picker import MDTimePicker  # versão antiga
+except:
+    from kivymd.uix.pickers import MDTimePicker  # versão nova
+
+
+class MainApp(MDApp):
+    def build(self):
+        return
+
+    def on_start(self):
+        time_picker = MDTimePicker()
+        time_picker.bind(time=self.hora_selecionada)
+        time_picker.open()
+
+    def hora_selecionada(self, instance, time):
+        print(f'Horário selecionado: {time}')
+
+
+MainApp().run()
